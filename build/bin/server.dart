552eb859cd7b600e2a026dfa0ba57c5d4498938e
Trey Hope <trey.a.hope@gmail.com>
2025-07-30 15:01:34 -0500

    Sure! Pl

---
+++ build/bin/server.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+
+import '../routes/index.dart' as index;
+
+
+void main() async {
+  final address = InternetAddress.anyIPv6;
+  final port = int.tryParse(Platform.environment['PORT'] ?? '8080') ?? 8080;
+  createServer(address, port);
+}
+
+Future<HttpServer> createServer(InternetAddress address, int port) async {
+  final handler = Cascade().add(buildRootHandler()).handler;
+  final server = await serve(handler, address, port);
+  print('\x1B[92m✓\x1B[0m Running on http://${server.address.host}:${server.port}');
+  return server;
+}
+
+Handler buildRootHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..mount('/', (context) => buildHandler()(context));
+  return pipeline.addHandler(router);
+}
+
+Handler buildHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..all('/', (context) => index.onRequest(context,));
+  return pipeline.addHandler(router);
+}
+
